7003154d60be58c2a34ae3d877287349c941281b
Keiran Kozlowski <keirankozlowski@gmail.com>
2023-09-06 10:19:16 -0400

    updated routes to include put and delete

--- server/db/helpers/pokemon.js
+++ server/db/helpers/pokemon.js
@@ -1,4 +1,5 @@
 const client = require("../client")
+const util = require('../util');
 
 const getAllPokemon = async () => {
     try {
@@ -48,4 +49,38 @@ const createPokemon = async ({ speciesId, name, trainerId, isFainted }) => {
     }
 }
 
-module.exports = { getAllPokemon, getPokemonById, createPokemon } 
+
+async function updatePokemon(pokemonId, fields) {
+    try {
+        const toUpdate = {}
+        for (let column in fields) {
+            if (fields[column] !== undefined) toUpdate[column] = fields[column];
+        }
+        let pokemon;
+
+        if (util.dbFields(toUpdate).insert.length > 0) {
+            const { rows } = await client.query(`
+            UPDATE pokemon
+            SET ${util.dbFields(toUpdate).insert}
+            WHERE "pokemonId"=${pokemonId}
+            RETURNING *;
+          `, Object.values(toUpdate));
+            pokemon = rows[0];
+        }
+
+        return pokemon;
+    } catch (error) {
+        throw error
+    }
+}
+
+async function deletePokemon(pokemonId) {
+    try {
+        const { rows } = await client.query('DELETE FROM pokemon WHERE "pokemonId"=$1 RETURNING *', [pokemonId]);
+        return rows[0];
+    } catch (err) {
+        throw err
+    }
+}
+
+module.exports = { getAllPokemon, getPokemonById, createPokemon, updatePokemon, deletePokemon } 
