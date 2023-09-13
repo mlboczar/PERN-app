1a57f312d9cc8031fcc8895641da3eb06fab8c4e
Keiran Kozlowski <keirankozlowski@gmail.com>
2023-09-13 16:31:00 -0400

    implementing authentication

--- server/db/client.js
+++ server/db/client.js
@@ -2,8 +2,8 @@
 const { Client } = require('pg')
 
 //Establishing connect to database (like how we do with http://)
-const pokedex = 'keirankozlowski'
-const client = new Client(`postgres://localhost:54321/${pokedex}`)
+const pokedex = 'pokedex'
+const client = new Client(`postgres://localhost:5432/${pokedex}`)
 
 //Export for use in other files
 module.exports = client;
