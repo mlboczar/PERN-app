3a2985ea3e4a1244f2e88f1d8c2ff14d8abafe19
Keiran Kozlowski <keirankozlowski@gmail.com>
2023-10-10 14:56:51 -0400

    deployment fixes

--- server/db/client.js
+++ server/db/client.js
@@ -3,7 +3,7 @@ const { Client } = require('pg')
 
 //Establishing connect to database (like how we do with http://)
 const pokedex = 'keirankozlowski'
-const client = new Client(`postgres://keirankozlowski_user:HsYrrINKUu7NsOsyBLYnJVDksH2btfBq@dpg-ckipnbolk5ic73d4hhr0-a.ohio-postgres.render.com/${pokedex}`)
+const client = new Client(`postgres://keirankozlowski_user:HsYrrINKUu7NsOsyBLYnJVDksH2btfBq@dpg-ckipnbolk5ic73d4hhr0-a/${pokedex}`)
 
 //Export for use in other files
 module.exports = client;
